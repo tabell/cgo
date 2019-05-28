90e8f3bdc7387a11f7610d98ef0a2417b5594cf3
Alex Bell <ab@cmd.com>
2019-05-28 07:57:36 -0700

    wip

--- ex6/main.go
+++ ex6/main.go
@@ -57,28 +57,21 @@ func findKeysize(in []byte) int {
 
 func foldBytes(in []byte, keysize int) (blocks []byte) {
     depth := len(in) / keysize
+    blocks = make([]byte, keysize)
     fmt.Printf("total=%d keysize=%d depth=%d\n", len(in), keysize, depth)
     for i := 0; i < keysize; i++ {
         slot := make([]byte, depth)
         for j := 0; j < depth; j++ {
-            slot[i] = in[j+keysize*i]
+            slot[j] = in[j*keysize+i]
         }
 
-        results := make([]utils.ScoredText, keySize))
-
-        for i,str := range inStrings 
-            in := utils.Hex2bytes(str)
-            result := utils.BestScore(in, keys)
-            results[i] = result
-        }
-
-        sort.Slice(results, func(i, j int) bool {
-            return results[i].Score < results[j].Score
-        })
+        bestKey := utils.FindRepeatingXorKey(slot)
+        blocks[i] = byte(bestKey.Key)
+        fmt.Println(bestKey.Score)
     }
+
     return
 }
-
 
 func main() {
     /* load base64 from file */
@@ -98,10 +91,14 @@ func main() {
     }
 
     /* guess at key size */
-    keysize := findKeysize(raw)
-    fmt.Println("found key size: ", keysize)
+    //keysize := findKeysize(raw)
+    keysize := 29
+        fmt.Println("found key size: ", keysize)
 
-    foldBytes(raw, keysize)
+        key := foldBytes(raw, keysize)
+        fmt.Println(string(key))
+
+        fmt.Println(string(utils.Xor(raw,key)))
 
 }
 
