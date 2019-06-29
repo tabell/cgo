c8d917ba7c1029651af4152fcbb1f95adde78a49
Alex <t.alexander.bell@gmail.com>
2019-06-28 22:18:59 -0700

    ex6 working
    - clean up repeating-xor

--- ex4/main.go
+++ ex4/main.go
@@ -7,10 +7,6 @@ import (
 )
 
 func main() {
-    keys := make([]rune, 256)
-    for i := range keys {
-        keys[i] = rune(i)
-    }
 
     inStrings := utils.StringArrayFromFile("ex4/input.txt")
 
@@ -18,14 +14,14 @@ func main() {
 
     for i,str := range inStrings {
         in := utils.Hex2bytes(str)
-        result := utils.BestScore(in, keys)
+        result := utils.FindRepeatingXorKey(in)
         results[i] = result
     }
 
     sort.Slice(results, func(i, j int) bool {
         return results[i].Score < results[j].Score
     })
+    winningResult := results[0]
 
-    result := results[0]
-    fmt.Printf("key=%s score=%.3f msg=%s\n", string(result.Key), result.Score, result.Text)
+    fmt.Printf("key=%c score=%.3f msg=%s\n", rune(winningResult.Key), winningResult.Score, winningResult.Text)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,151 +1,159 @@
 package utils
 
 import (
-    "fmt"
-    "encoding/base64"
-    "encoding/hex"
-    "unicode"
-    "math"
-    "sort"
-    "os"
-    "log"
-    "bufio"
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
 )
 
 type ScoredText struct {
-    Key rune
-    Text string
-    Score float64
+	Key   byte
+	Text  string
+	Score float64
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func Xor(a []byte, b []byte) []byte {
-    j := len(a)
-    k := len(b)
-    var out = make([]byte, max(j,k))
-    for i,_ := range a {
-        out[i] = a[i % j] ^ b[i % k]
-    }
+	j := len(a)
+	k := len(b)
+	var out = make([]byte, max(j, k))
+	for i, _ := range a {
+		out[i] = a[i%j] ^ b[i%k]
+	}
 
-    return out
+	return out
 }
 
 func makeStdFreq() (table [256]float64) {
-    table[int(' ')] =  0.05
-    table[int('A')] =  0.08167
-    table[int('B')] =  0.01492
-    table[int('C')] =  0.02782
-    table[int('D')] =  0.04253
-    table[int('E')] =  0.12702
-    table[int('F')] =  0.02228
-    table[int('G')] =  0.02015
-    table[int('H')] =  0.06094
-    table[int('I')] =  0.06966
-    table[int('J')] =  0.00153
-    table[int('K')] =  0.00772
-    table[int('L')] =  0.04025
-    table[int('M')] =  0.02406
-    table[int('N')] =  0.06749
-    table[int('O')] =  0.07507
-    table[int('P')] =  0.01929
-    table[int('Q')] =  0.00095
-    table[int('R')] =  0.05987
-    table[int('S')] =  0.06327
-    table[int('T')] =  0.09056
-    table[int('U')] =  0.02758
-    table[int('V')] =  0.00978
-    table[int('W')] =  0.02360
-    table[int('X')] =  0.00150
-    table[int('Y')] =  0.01974
-    table[int('Z')] =  0.00074
-    return table
+	table[int(' ')] = 13
+	table[int('A')] = 08.167
+	table[int('B')] = 01.492
+	table[int('C')] = 02.782
+	table[int('D')] = 04.253
+	table[int('E')] = 12.702
+	table[int('F')] = 02.228
+	table[int('G')] = 02.015
+	table[int('H')] = 06.094
+	table[int('I')] = 06.966
+	table[int('J')] = 00.153
+	table[int('K')] = 00.772
+	table[int('L')] = 04.025
+	table[int('M')] = 02.406
+	table[int('N')] = 06.749
+	table[int('O')] = 07.507
+	table[int('P')] = 01.929
+	table[int('Q')] = 00.095
+	table[int('R')] = 05.987
+	table[int('S')] = 06.327
+	table[int('T')] = 09.056
+	table[int('U')] = 02.758
+	table[int('V')] = 00.978
+	table[int('W')] = 02.360
+	table[int('X')] = 00.150
+	table[int('Y')] = 01.974
+	table[int('Z')] = 00.074
+	return table
 }
 
-
 func computeFrequencies(text []byte) (table [256]float64) {
-    for i := range text {
-        text[i] = byte(unicode.ToUpper(rune(text[i])))
-    }
-    count := 0
-    for i := range text {
-        count++
-        table[int(text[i])]++
-    }
-    for i := range text {
-        table[int(text[i])] /= float64(count)
-    }
-    return
+	for i := range text {
+		char := text[i]
+		if char >= 97 && char <= 122 {
+			char -= 32
+		}
+		table[int(char)]++
+	}
+	return
 }
 
 func testDistance() {
-    in := []byte("The quick brown fox jumps over the lazy dog's back")
-    fmt.Println(scoreText(in), string(in))
-    in = []byte("Tomorrow, you will be released. If you are bored of brawling with thieves and want to achieve something there is a rare blue flower that grows on the eastern slopes. Pick one of these flowers. If you can carry it to the top of the mountain, you may find what you were looking for in the first place.")
-    fmt.Println(scoreText(in), string(in))
-    in = Hex2bytes("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-    fmt.Println(scoreText(in), string(in))
+	in := []byte("The quick brown fox jumps over the lazy dog's back")
+	fmt.Println(scoreText(in), string(in))
+	in = []byte("Tomorrow, you will be released. If you are bored of brawling with thieves and want to achieve something there is a rare blue flower that grows on the eastern slopes. Pick one of these flowers. If you can carry it to the top of the mountain, you may find what you were looking for in the first place.")
+	fmt.Println(scoreText(in), string(in))
+	in = Hex2bytes("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	fmt.Println(scoreText(in), string(in))
 }
 
 func distance(a [256]float64, b [256]float64) (distance float64) {
-    for x := range a {
-        distance += math.Sqrt(math.Abs(a[x]*a[x] - b[x]*b[x]))
-    }
-    return
+	for x := range a {
+		d := math.Sqrt(math.Abs(a[x]*a[x] - b[x]*b[x]))
+		distance += d
+	}
+	return
 }
 
-func BestScore(in []byte, keys []rune) ScoredText {
-    scores := make([]ScoredText, len(keys))
-    for i := range keys {
-        scores[i].Key = keys[i]
-        scores[i].Text = string(Xor(in, []byte(string(scores[i].Key))))
-        scores[i].Score = scoreText([]byte(scores[i].Text))
-    }
-    sort.Slice(scores, func(i, j int) bool {
-        return scores[i].Score < scores[j].Score
-    })
+func FindRepeatingXorKey(in []byte) ScoredText {
+	/* todo: make this table static */
+	keys := make([]rune, 256)
+	for i := range keys {
+		keys[i] = rune(i)
+	}
 
-    return scores[0]
+	/* decode the string with each key and score the result */
+	scores := make([]ScoredText, len(keys))
+	for i := range keys {
+		//scores[i].Text = string(Xor(in, []byte(string(scores[i].Key))))
+		k := make([]byte, 1)
+		scores[i].Key = byte(i)
+		k[0] = scores[i].Key
+		scores[i].Text = string(Xor(in, k))
+		scores[i].Score = scoreText([]byte(scores[i].Text))
+		//(%c) --> score = %f: key=%c decoded=%s",
+		//	i, i, scores[i].Score, scores[i].Key, scores[i].Text)
+	}
+	sort.Slice(scores, func(i, j int) bool {
+		return scores[i].Score < scores[j].Score
+	})
+
+	//log.Printf(scores[0].Text)
+
+	return scores[0]
 }
 
 func Hex2bytes(ins string) []byte {
-    in := []byte(ins)
-    raw := make([]byte, hex.DecodedLen(len(in)))
-    hex.Decode(raw, in)
-    return raw
+	in := []byte(ins)
+	raw := make([]byte, hex.DecodedLen(len(in)))
+	hex.Decode(raw, in)
+	return raw
 }
 
 func Hex2b64(str string) []byte {
-    raw := Hex2bytes(str)
-    encoded := base64.StdEncoding.EncodeToString(raw)
-    return []byte (encoded)
+	raw := Hex2bytes(str)
+	encoded := base64.StdEncoding.EncodeToString(raw)
+	return []byte(encoded)
 }
 
 func scoreText(in []byte) float64 {
-    return math.Abs(1 - distance(makeStdFreq(), computeFrequencies(in)))
+	return math.Abs(1 - distance(makeStdFreq(), computeFrequencies(in)))
 }
 
 func StringArrayFromFile(filename string) (result []string) {
-    file,err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        result = append(result, scanner.Text())
-    }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		result = append(result, scanner.Text())
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-    return
+	return
 }
-
